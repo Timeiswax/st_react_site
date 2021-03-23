696fb13b77e6bcc21f58a1c058049342afe82f0f
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2021-03-23 18:56:31 -0500

    ???

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,6 @@
 import '../styles/newMain.css'
 import { useRouter } from 'next/router'
 import { CSSTransition, TransitionGroup} from 'react-transition-group'
-// import ReactPixel from 'react-facebook-pixel'
-
-// const pixID = '509089730085163'
-
-// function FacebookPixel() {
-//   React.useEffect(() => {
-//     import("react-facebook-pixel")
-//       .then((x) => x.default)
-//       .then((ReactPixel) => {
-//         ReactPixel.init('pixel ID here');
-//         ReactPixel.pageView();
-
-//         Router.events.on("routeChangeComplete", () => {
-//           ReactPixel.pageView();
-//         });
-//       });
-//   });
-//   return null;
-// }
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
