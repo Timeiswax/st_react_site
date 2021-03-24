4daf831317c605ecfa14fd830e18520db28acd1e
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2021-03-23 21:44:44 -0500

    quar 2

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ const index = () => {
     return ( 
     <div className="full-page">
         <div className="col" style={{textAlign:"center"}}>            
-            <h1>This site is under construction right now</h1>
-            <h2>But you can get to my page for Setup:</h2>
+            <h3>This site is under construction right now</h3>
+            <h3>But you can get to my page for Setup:</h3>
             <Link href='/music/Setup' >Here</Link>
         </div>
     </div>
