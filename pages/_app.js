8b65a5b7fd5d871dd8b3dfa36547dbd1ae8233b2
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2021-04-22 07:48:20 -0400

    with fb verification head

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/newMain.css'
 import { useRouter } from 'next/router'
 import { CSSTransition, TransitionGroup} from 'react-transition-group'
 import {useEffect} from 'react'
-
+import Head from 'next/head'
 import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
 
@@ -11,16 +11,16 @@ function MyApp({ Component, pageProps }) {
 
 
   
-  const analytics = Analytics({
-    app: 'awesome-app',
-    plugins: [
-      googleAnalytics({
-        trackingId: 'UA-149635073-1'
-      })
-    ]
-  })
+  // const analytics = Analytics({
+  //   app: 'awesome-app',
+  //   plugins: [
+  //     googleAnalytics({
+  //       trackingId: 'UA-149635073-1'
+  //     })
+  //   ]
+  // })
 
-  analytics.page()
+  // analytics.page()
 
   // useEffect(() => {
   //   import('react-facebook-pixel')
@@ -32,16 +32,22 @@ function MyApp({ Component, pageProps }) {
   // }, [])
 
   return(
-    <TransitionGroup>
-      <CSSTransition
-      appear={true}
-      key={router.pathname}
-      timeout={1000}
-      classNames={"fullPageTransition"}
-    >
-          <Component {...pageProps} />
-      </CSSTransition>
-    </TransitionGroup>
+    <div>
+        <Head>
+          <meta name="facebook-domain-verification" content="2a89t8tk3isn5rb7ms50t3n5542w4f" />
+        </Head>
+        <TransitionGroup>
+          <CSSTransition
+          appear={true}
+          key={router.pathname}
+          timeout={1000}
+          classNames={"fullPageTransition"}
+        >
+              <Component {...pageProps} />
+          </CSSTransition>
+        </TransitionGroup>
+    </div>
+
   )
 }
 
